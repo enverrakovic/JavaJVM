22c2bda6b996b57e7535a2f34d37601d0b7ca4c8
Enver Raković <enver.rakovic@gmail.com>
2026-07-01 20:47:03 +0200

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        // "Entry point" programa
+        // Tijelo metode main:
+        System.out.println("Hello World!");
+    }
+
+}
